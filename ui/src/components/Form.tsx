2e5236e7e4593d85af01774a869ec8398f739288
mo3789530 <mo3789530@gmail.com>
2023-03-12 21:21:03 +0900

    wip

---
+++ ui/src/components/Form.tsx
@@ -0,0 +1,38 @@
+import { Table, TextField } from "@mui/material";
+import * as React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <Table  style={{ width: '30%', height: '30%' }} >
+        <h3>Project Info</h3>
+        <tr>
+          <th>
+            <label>Name: </label>
+          </th>
+          <th>
+            <TextField id="project-name" label="Name" variant="standard" />
+          </th>
+        </tr>
+        <tr>
+          <th>
+            <label>Region: </label>
+          </th>
+          <th>
+            <TextField id="project-region" label="Region" variant="standard" />
+          </th>
+        </tr>
+        <tr>
+          <th>
+            <label>Env: </label>
+          </th>
+          <th>
+            <TextField id="project-env" label="Env" variant="standard" />
+          </th>
+        </tr>
+      </Table>
+    </div>
+  )
+}
+
+export default Form;
---
+++ ui/src/components/OptionList.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const OptionList = () => { };
+
+export default OptionList;

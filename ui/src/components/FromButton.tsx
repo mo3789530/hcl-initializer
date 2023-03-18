b1aa3b02f937873954c3499fc856f8d98290eb45
mo3789530 <mo3789530@gmail.com>
2023-03-18 18:16:56 +0900

    wip

---
+++ ui/src/components/FromButton.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+import * as React from "react";
+
+const FromButton = () => {
+    return (
+        <Button variant="contained">Contained</Button>
+    )
+}
